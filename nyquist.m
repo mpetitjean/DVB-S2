93a4971841bd8cb2b2cbfd552077040aaf6aa1d5
Mathieu <petitjean.ma@gmail.com>
2017-03-20 20:25:25 +0100

    nyquist cheat looksok but weird

--- nyquist.m
+++ nyquist.m
@@ -2,7 +2,7 @@
 clear all; close all;
 taps = 101;
 beta = 0.3;
-f_samp = 3e6;
+f_samp = 4e6;
 f_symb = 1e6;
 T = 1/f_symb;
 result = zeros(1, taps);
@@ -23,8 +23,9 @@ end
 figure;
 stem(result)
 result_t = ifft(result);
-result_t = [result_t(52:end) result_t(1:51)];
-result_t = result_t./result_t(51);
+result_t = [result_t((taps/2)+0.5:end) result_t(1:(taps/2)-0.5)];
+result_t = result_t./result_t((taps/2)-0.5);
 figure;
 t = (-(taps - 1) / 2 : (taps - 1) / 2)./f_samp;
 plot(t,result_t)
+
