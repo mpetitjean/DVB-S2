2a09c5ecbd222216b0b0a1230fd81701e93992d0
f380cedric@users.noreply.github.com <f380cedric@users.noreply.github.com>
2017-03-20 18:06:57 +0100

    Switch output of ifft, solution?

--- nyquist.m
+++ nyquist.m
@@ -1,7 +1,7 @@
 %function  result_t = nyquist(taps, beta)
 clear all; close all;
-taps = 1001;
-beta = 0.5;
+taps = 101;
+beta = 0.3;
 f_samp = 3e6;
 f_symb = 1e6;
 T = 1/f_symb;
@@ -13,16 +13,18 @@ lcorner = (1 - beta) / (2 * T);
 rcorner = (1 + beta) / (2 * T);
 for i = 1:1:taps
 	if (abs(f(i)) < lcorner)
-		result(i) = sqrt(T)/sqrt(T);
+		result(i) = sqrt(T);
 	elseif (abs(f(i)) > rcorner)
 		result(i) = 0;
     else
-        result(i) = sqrt(T / 2 * (1 + cos(pi * T / beta * (abs(f(i)) - lcorner)))) /sqrt(T) ;
-	end
+        result(i) = sqrt(T / 2 * (1 + cos(pi * T / beta * (abs(f(i)) - lcorner))));
+    end 
 end
 figure;
 stem(result)
 result_t = ifft(result);
+result_t = [result_t(52:end) result_t(1:51)];
+result_t = result_t./result_t(51);
 figure;
 t = (-(taps - 1) / 2 : (taps - 1) / 2)./f_samp;
 plot(t,result_t)
