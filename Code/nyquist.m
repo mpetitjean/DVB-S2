35d236bf3aa61e09ee96cb01ca7254cdb762e7a2
f380cedric <f380cedric@users.noreply.github.com>
2017-03-22 21:28:07 +0100

    ber for Nbps = 2, 4, 6, 8

--- Code/nyquist.m
+++ Code/nyquist.m
@@ -7,14 +7,14 @@ highf = stepo * (taps - 1)/2;
 f = linspace(-highf, highf, taps);
 lcorner = (1 - beta) / (2 * T);
 rcorner = (1 + beta) / (2 * T);
-for i = 1:1:taps
-	if (abs(f(i)) < lcorner)
-		result(i) = sqrt(T);
+parfor i = 1:1:taps
+    if (abs(f(i)) < lcorner)
+        result(i) = sqrt(T);
 	elseif (abs(f(i)) > rcorner)
 		result(i) = 0;
     else
         result(i) = sqrt(T / 2 * (1 + cos(pi * T / beta * (abs(f(i)) - lcorner))));
-    end 
+   end 
 end
 
 %figure;
