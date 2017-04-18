1c5c1503aa76ddfbc7544ea564a159c88c135a4a
f380cedric <f380cedric@users.noreply.github.com>
2017-04-18 17:36:23 +0200

    oups

--- Code/tanner.m
+++ Code/tanner.m
@@ -3,5 +3,5 @@ function bloc_o = tanner(bloc_in,H)
     Perform one iteration of hard decoding for the tanner graph
 %}
     % Compute error pattern
-    error = mod(H*bloc_in',2);
-    bloc_o =  (sum(xor(error,bloc_in).*H) > sum(H)/2);
+    syndrom = mod(H*bloc_in',2);
+    bloc_o =  (sum(xor(syndrom,bloc_in).*H) > sum(H)/2);
