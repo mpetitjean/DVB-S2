2035ec3c4415dacafc2b91aceb4649b3763ff72f
f380cedric <f380cedric@users.noreply.github.com>
2017-04-22 11:50:37 +0200

    little improvement for LDPDecode

--- Code/LDPCDecode.m
+++ Code/LDPCDecode.m
@@ -4,15 +4,14 @@ function bits_info = LDPCDecode(coded_bits, H, maxit)
 sizeC = max(size(H));
 sizeF = min(size(H));
 ratio = sizeC/sizeF;
-
-bits_info = zeros(1, length(coded_bits)/ratio);
+l_bits = length(coded_bits);
+bits_info = zeros(1, l_bits/ratio);
 % take bloc per bloc
-for startIndex = 1:sizeC:length(coded_bits)
+for startIndex = 1:sizeC:l_bits
     bloc_in = coded_bits(startIndex:startIndex+sizeC-1);
-    
     iter = 1;
     bloc_o = tanner(bloc_in, H);
-    
+
     % Iterate on tanner graph while not converged
     while  ~isequal(bloc_in,bloc_o) && (iter < maxit)
         bloc_in = bloc_o;
