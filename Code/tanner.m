1b082ec4b4a2996cc3d58e166a5233a2cfdec9be
Mathieu <petitjean.ma@gmail.com>
2017-05-03 15:11:26 +0200

    bbkblb

--- Code/tanner.m
+++ Code/tanner.m
@@ -3,4 +3,4 @@ function bloc_o = tanner(y, syndrom, m,H)
     Perform one iteration of hard decoding for the tanner graph
 %}
     % Compute error pattern
-    bloc_o = (sum(xor(syndrom,m).*H) + y) > sum(H)/2;
+bloc_o = (sum(xor(syndrom,m).*H) + y) > sum(H)/2;
