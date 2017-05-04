aa8d44d4885583d7d037ad175978be32a8f27472
f380cedric <f380cedric@users.noreply.github.com>
2017-05-04 09:20:12 +0200

    main_step3 updated

--- Code/main_step3.m
+++ Code/main_step3.m
@@ -1,6 +1,6 @@
 % clear;
 % close all;
-function ber = main_step1(Nbps,precision, ratio_min, step, ratio_max)
+function ber = main_step3(Nbps,precision, ratio_min, step, ratio_max)
 % Simulation parameters
 f_sym 	= 2e6;
 f_samp	= 8e6;
@@ -17,14 +17,17 @@ rolloff = 0.3;
 
 % Message generation
 bits = randi([0 1],[1 N]);     % random bits generation
- 
+disp('Bits generated')
+
 % Mapping
 if(Nbps > 1)
     symb_tx = mapping(bits,Nbps,'qam').';
+    mode = 'qam';
 else
     symb_tx = mapping(bits,Nbps,'pam').';
+    mode = 'qam';
 end
-    
+disp('Mapping done')
 % 
 % figure;
 % plot(symb_tx(1,:), 'x');
@@ -33,34 +36,40 @@ end
 
 % Upsampling
 message_symb = upsample(symb_tx, f_samp/f_sym);
+disp('Upsample done')
+clear symb_tx
 
 % Nyquist
 nyquist_impulse = nyquist(taps, rolloff, f_samp, f_sym);
 message_symb_n = conv(message_symb, nyquist_impulse);
+disp('First RRC filter done')
+clear message_symb
 
 E_b = 1/(2*f_samp*N)*(trapz(abs(message_symb_n).^2));
 
 % Add noise
 message_noisy = noise(message_symb_n, ratio_min, step, ratio_max, f_samp, E_b);
-%message_noisy = message_symb_n;
+disp('Noise added')
+clear message_symb_n
 
 % Nyquist
 num = size(message_noisy,1);
 message_noisy_n = zeros(num, length(message_noisy)+length(nyquist_impulse)-1);
 normalization = max(real(conv(nyquist_impulse, nyquist_impulse)));
+stem(nyquist_impulse);
 parfor i = 1:num
 	message_noisy_n(i,:) = conv(message_noisy(i,:), fliplr(nyquist_impulse))./normalization;
 end
+disp('Second RRC filter done')
+clear message_noisy normalization nyquist_impulse
 
 % Drop meaningless samples
 message_noisy_n = message_noisy_n(:,taps:end-(taps-1));
 
 % Downsampling
-symb_rx = zeros(num, length(symb_tx));
-parfor i = 1:num
-    %symb_rx(i,:) = undersampling(message_noisy_n(i,:), f_sym, f_samp);
-    symb_rx(i,:) = downsample(message_noisy_n(i,:), f_samp/f_sym);
-end
+symb_rx= downsample(message_noisy_n.', f_samp/f_sym).';
+disp('Downsampling done')
+clear message_noisy_n
 
 % figure;
 % plot(symb_rx(1,:), 'x');
@@ -68,14 +77,17 @@ end
 % grid on;
 
 % Demapping
-bits_rx = zeros(num, length(bits));
-parfor i = 1:num
-    if(Nbps > 1)
-        bits_rx(i,:) = demapping(symb_rx(i,:).',Nbps,'qam');
-    else
-        bits_rx(i,:) = demapping(real(symb_rx(i,:)).',Nbps,'pam');
-    end
+bits_rx = zeros(num,length(bits));
+if strcmp(mode,'pam')
+    symb_rx = real(symb_rx);
 end
+
+symb_rx = symb_rx.';
+parfor i = 1:num
+        bits_rx(i,:) = demapping(symb_rx(:,i),Nbps,mode);
+end
+disp('Demapping done')
+clear symb_rx
 
 % Compute BER and plot
 ber = compute_ber(bits, bits_rx, num);
